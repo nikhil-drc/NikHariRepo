dba379e367e852cb445601d4bf37fb22743a938e
Nikhilchandra Rathod <nikhil.rathod@drcinfotech.com>
2013-09-20 14:32:50 +0530

    Initialize Project - Nik

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('MyAppApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
