7c0676a6a2b60aff3f8c15f2923ea198cdeb3c78
Nikhilchandra Rathod <nikhil.rathod@drcinfotech.com>
2013-09-20 14:55:04 +0530

    Initialize todo app - Nik

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,11 @@
 
 angular.module('MyAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+      $scope.todos = [
+        "Item1",
+        "Item2",
+        "Item3"
+      ];
+      
   });
